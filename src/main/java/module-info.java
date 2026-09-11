c5045afa9ef57bd234fa8f66a6c9f6abff54a120
DasaniDealer <freemangre@gmail.com>
2026-09-11 13:27:21 -0700

    release-v1.0.0

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.paintfx.freemanpaintfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.paintfx.freemanpaintfx to javafx.fxml;
+    exports com.paintfx.freemanpaintfx;
+}
